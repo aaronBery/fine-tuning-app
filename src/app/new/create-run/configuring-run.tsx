8381892b335a46ad6fbcfa023df77a59c74dec0c
Aaron Bery <aaronaldo99@gmail.com>
2025-05-21 15:36:55 +0100

    add validation

--- src/app/new/create-run/configuring-run.tsx
+++ src/app/new/create-run/configuring-run.tsx
@@ -1,10 +1,15 @@
 import { NewJobContext } from "@/context/new-job.context";
 import { Stages } from "@/models/stages.enum";
 import { Button, FormControl, Input, InputLabel } from "@mui/material";
-import { FormEvent, useContext } from "react";
+import { FormEvent, useContext, useState } from "react";
 
 export const ConfiguringRun = () => {
     const { newJob, setCurrentStep, updateEpochs, updateWarmupEpochs, updateEvaluationEpochs, updateLearningRate } = useContext(NewJobContext);
+    const [ epochTouched, setEpochTouched ] = useState(false);
+    const [ warmupEpochsTouched, setWarmupEpochsTouched ] = useState(false);
+    const [ evaluationEpochsTouched, setEvaluationEpochsTouched ] = useState(false);
+    const [ learningRateTouched, setLearningRateTouched ] = useState(false);
+
 
     const submit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -13,6 +18,14 @@ export const ConfiguringRun = () => {
     };
 
     const formControlSx = { marginBottom: 5};
+
+    const isValidEpoch = () => newJob.epochs > 0;
+
+    const isValidEvaluationEpoch = () => newJob.evaluationEpochs <= newJob.epochs;
+
+    const isValidWarmupEpoch = () => newJob.warmupEpochs <= newJob.epochs;
+
+    const isValidLearningRate = () => parseInt(newJob.learningRate, 10) >= 0 && parseInt(newJob.learningRate, 10) <= 1;
 
     return (
         <>
@@ -23,26 +36,37 @@ export const ConfiguringRun = () => {
                 <div className="grid grid-cols-1">
                     <FormControl sx={formControlSx}>
                         <InputLabel htmlFor="ephochs">Epochs</InputLabel>
-                        <Input id="ephochs" type="number" name="ephochs" value={newJob.epochs} onChange={(e) => updateEpochs(e.target.value)} />
+                        <Input id="ephochs" type="number" name="ephochs" value={newJob.epochs} onChange={(e) => updateEpochs(e.target.value)}
+                            error={epochTouched && !isValidEpoch()}
+                            onFocus={() => setEpochTouched(true)} />
                     </FormControl>
 
                     <FormControl sx={formControlSx}>
                         <InputLabel htmlFor="warmupEpochs">Warmup Epochs</InputLabel>
-                        <Input id="warmupEpochs" type="number" name="warmupEpochs" value={newJob.warmupEpochs} onChange={(e) => updateWarmupEpochs(e.target.value)} />
+                        <Input id="warmupEpochs" type="number" name="warmupEpochs" value={newJob.warmupEpochs} onChange={(e) => updateWarmupEpochs(e.target.value)}
+                            error={warmupEpochsTouched && !isValidWarmupEpoch()}
+                            onFocus={() => setWarmupEpochsTouched(true)} />
                     </FormControl>
                 </div>
                 <div className="grid grid-cols-1">
                     <FormControl sx={formControlSx}>
                         <InputLabel htmlFor="evaluationEpochs">Evaluation Epochs</InputLabel>
-                        <Input id="evaluationEpochs" type="number" name="evaluationEpochs" value={newJob.evaluationEpochs} onChange={(e) => updateEvaluationEpochs(e.target.value)} />
+                        <Input id="evaluationEpochs" type="number" name="evaluationEpochs" value={newJob.evaluationEpochs} onChange={(e) => updateEvaluationEpochs(e.target.value)}
+                            error={evaluationEpochsTouched && !isValidEvaluationEpoch()}
+                            onFocus={() => setEvaluationEpochsTouched(true)} />
                     </FormControl>
 
                     <FormControl sx={formControlSx}>
                         <InputLabel htmlFor="learningRate">Learning rate</InputLabel>
-                        <Input id="learningRate" name="learningRate" value={newJob.learningRate} onChange={(e) => updateLearningRate(e.target.value)} />
+                        <Input id="learningRate" name="learningRate" value={newJob.learningRate} onChange={(e) => updateLearningRate(e.target.value)} 
+                            error={learningRateTouched && !isValidLearningRate()}
+                            onFocus={() => setLearningRateTouched(true)} />
                     </FormControl>
                 </div>
-                <Button type="submit">Next: Review</Button>
+                <Button type="submit" 
+                    disabled={!isValidEpoch() || !isValidEvaluationEpoch() || !isValidWarmupEpoch() || !isValidLearningRate()}>
+                    Next: Review
+                </Button>
             </form>
         </>
     );
--- src/app/new/create-run/set-up-run.tsx
+++ src/app/new/create-run/set-up-run.tsx
@@ -3,7 +3,7 @@
 import { NewJobContext } from "@/context/new-job.context";
 import { BaseModel } from "@/models/job.model";
 import { Stages } from "@/models/stages.enum";
-import { Button, FormControl, Input, InputLabel, MenuItem, Select } from "@mui/material";
+import { Button, FormControl, Input, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { FormEvent, useContext, useState } from "react";
 
 interface SetUpRunProps {
@@ -17,25 +17,40 @@ export interface setUpFormModel {
 
 export function SetUpRun({ baseModelOptions = [] } : SetUpRunProps) {
     const { newJob, updateName, updateBaseModel, setCurrentStep } = useContext(NewJobContext);
+    const [ nameTouched, setNameTouched ] = useState(false);
+    const [ baseModelTouched, setBaseModelTouched ] = useState(false);
 
     const submit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        if (!isNameValid() || !isBaseModelValid()) {
+            return;
+        }
+
         setCurrentStep(Stages.CONFIGURE);
     }
 
+    const isNameValid = () => newJob.name.length > 2 && newJob.name.length < 51 && /^[A-Z0-9\-]+$/gi.test(newJob.name);
+
+    const isBaseModelValid = () => newJob.baseModel.length > 0;
+
     return (
         <>
-            <h1>Set up your run</h1>
+            <h1 className="mb-5">Set up your run</h1>
             <form onSubmit={(e) => submit(e)} className="flex flex-column">
                 <FormControl>
-                    <InputLabel htmlFor="name">Name</InputLabel>
-                    <Input id="name" name="name" value={newJob.name} onChange={(e) => updateName(e.target.value)} />
+                    <InputLabel htmlFor="name">Name your job</InputLabel>
+                    <TextField id="name" name="name" value={newJob.name} onChange={(e) => updateName(e.target.value)}
+                        error={nameTouched && !isNameValid()}
+                        onFocus={() => setNameTouched(true)}
+                        helperText="3-50 alpha numeric characters along dashes are permitted." />
                 </FormControl>
 
                 <FormControl>
                     <InputLabel id="baseModel">Select base model</InputLabel>
                     <Select labelId="baseModel" name="baseModel" value={newJob.baseModel} sx={{minWidth: '200px'}}
+                        error={baseModelTouched && !isBaseModelValid()}
+                        onFocus={() => setBaseModelTouched(true)}
                         onChange={(e) => updateBaseModel(e.target.value)}>
                         {baseModelOptions.map(option => 
                              <MenuItem key={option.id} value={option.id}>{option.displayName}</MenuItem>
@@ -43,7 +58,7 @@ export function SetUpRun({ baseModelOptions = [] } : SetUpRunProps) {
                     </Select>
                 </FormControl>
 
-                <Button type="submit">Next: Configure</Button>
+                <Button type="submit" disabled={!isNameValid() || !isBaseModelValid()}>Next: Configure</Button>
             </form>
         </>
     )
